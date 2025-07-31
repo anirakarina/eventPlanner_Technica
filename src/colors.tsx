e96c9ad43019b5a5e2ce7ff34735d7d6b319bf1b
anirakarina <93230939+anirakarina@users.noreply.github.com>
2025-07-31 18:31:21 -0400

    created colors reference

---
+++ src/colors.tsx
@@ -0,0 +1,20 @@
+//colors to be used throughout the site
+const theme = {
+    dark: '#130F1E',
+    text: '#FFFFFF',
+    background: '#25233F',
+    technicaBlue: '#10F7F8',
+    technicaPink: '#FA7FAE',
+    trackGeneral: '#98C7D5',
+    trackBeginner: '#FFAD59',
+    trackStartup: '#B396D1',
+    trackResearch: '#CE9898'
+}
+
+const colors = theme;
+
+export default colors;
+
+//Using colors:
+//import colors from "@/src/colors";
+//fillColorRandomObject: colors.technicaBlue
