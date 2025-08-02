de66a91a510c1c96021bbd40616fc4a2ceebf9b4
Illusioner <tomo701may@gmail.com>
2025-08-01 23:16:35 -0400

    Add example workshops and calendar days

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,56 @@ import Calendar from '/components/calendar.jsx';
 import Workshop from '/components/workshop.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
+  const Sat_workshops = [
+    {
+      title: "React 101",
+      time: "9AM-10AM",
+      tags: ["beginner", "front-end dev", "javascript", "CSS", "HTML", "React"],
+      desc: "An introduction to creating apps with React and nodejs! No background in front end development necessary!"
+    },
+    {
+      title: "Networking for business",
+      time: "3PM-4PM",
+      tags: ["Entrepreneurship", "Startup", "Networking"],
+      desc: "Want to increase your connections? This workshop will have tips on expanding your outreach using LinkedIn and Handshake, as well as having a QnA time at the end from our Technica mentors!"
+    }
+  ];
+
+  const Sun_workshops = [
+    {
+      title: "Computer Science research",
+      time: "8PM-9PM",
+      tags: ["Computer Science", "Interdisciplinary", "Research"],
+      desc: "This workshop will introduce you to the various fields of research within computer science and its interdisciplinary areas, as well as give advice as to how to find research opportunities!"
+    },
+    {
+      title: "Working with Django",
+      time: "4PM-5PM",
+      tags: ["Back-end dev", "beginner", "Django", "Python"],
+      desc: "An introduction to backend development using the python framework Django! No backend development experience required."
+    }
+  ];
+
+  const Sat_calendar = {
+    day: "Saturday"
+  };
+
+  const Sun_calendar = {
+    day: "Sunday"
+  };
 
   return (
     <>
-      <div>
+      
+      
+    </>
+  )
+}
+
+export default App
+
+{/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -29,9 +74,4 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+      </p> */}
