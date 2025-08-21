8c3707de92268f83610fdd534ffead2a2c1e0926
Illusioner <tomo701may@gmail.com>
2025-08-21 09:49:13 -0400

    Fix router, starts on full schedule page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import './App.css'
 import FullSchedule from './pages/FullSchedule'
 import MySchedule from './pages/MySchedule';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
 
   return (
-    <>
-      <FullSchedule />
-      <MySchedule />
-      {/* I just put both pages to see them both, you can remove whichever to view one at a time*/}
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<FullSchedule/>}/>
+        <Route path="/MySchedule" element={<MySchedule/>}/>
+      </Routes>
+    </BrowserRouter>
+    // <>
+    //   <FullSchedule />
+    //   <MySchedule />
+    //   {/* I just put both pages to see them both, you can remove whichever to view one at a time*/}
+    // </>
   )
 }
 
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,13 +1,14 @@
 import '../App.css';
 import { CgProfile } from "react-icons/cg";
+import {Link} from "react-router-dom";
 
 const Navbar = () => {
 
     return (
     <div className='navbar'>
       <div className='navlinks'>
-        <div>Full Schedule</div>
-        <div>My Schedule</div>
+        <Link to="/">Full Schedule</Link>
+        <Link to="/MySchedule">My Schedule</Link>
       </div>
       <CgProfile className='profile-icon'/>
     </div>
